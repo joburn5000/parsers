6731bc61aabccc2b5372dbab5f1c385435ee66d0
unknown <newburn2@illinois.edu>
2024-06-22 14:11:52 -0400

    implement pdf_reader and rename pdfplumber

--- pipeline.py
+++ pipeline.py
@@ -34,7 +34,7 @@ pdfreader.extract = pdfreader_extract
 PDFMiner.extract = PDFMiner_extract
 
 pdf_parsers = [pdf_plumber, pdfreader, PyPDF2, camelot, Amazon_Textract, Microsoft_Table_Transformer, llama2parser, tabula_py, pdf2tables, PDFMiner]
-test_pdf_parsers = [pdfreader]
+test_pdf_parsers = [camelot, tabula_py]
 dataset = dataset()
 metrics = metrics()
 
