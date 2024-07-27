9cf42db420399b4790130388b797a0982ff3abf8
unknown <newburn2@illinois.edu>
2024-07-27 07:17:41 -0500

    add speed evaluaion

--- pipeline.py
+++ pipeline.py
@@ -19,31 +19,39 @@
 # output results in a comprehensible manner
 
 import os
+import datetime #TODO add to requirements.txt
 from utils import *
 from wrappers import pdf_plumber, PyPDF2, PyPDF, pdf_2_tables, tabula_py, PDFMiner, camelot, llama_parse, Amazon_Textract, Microsoft_Table_Transformer
 
 pdf_parsers = [pdf_plumber, PyPDF, PyPDF2, camelot, Amazon_Textract, Microsoft_Table_Transformer, llama_parse, tabula_py, pdf_2_tables, PDFMiner]
-test_pdf_parsers = []
-dataset = dataset()
+test_pdf_parsers = [pdf_plumber]
 
 # initialize data: get pdfs from dataset folder
 Arxiv_papaers_directory = "dataset/Arxiv_papers"
 public_SEC_docs_directory = "dataset/public_SEC_docs"
 Arxiv_files = os.listdir(Arxiv_papaers_directory)
 public_SEC_pdfs = os.listdir(public_SEC_docs_directory)
+dataset = dataset()
 for pdf in Arxiv_files:
     dataset.pdfs.append(Arxiv_papaers_directory+"/"+pdf)
 for pdf in public_SEC_pdfs:
     dataset.pdfs.append(public_SEC_docs_directory+"/"+pdf)
 
+# initialize metrics for each parser
+for parser in pdf_parsers:
+    parser.metrics = metrics()
+
 #retrieve data for each pdf
 results = []
 total_extracted_data = []
 for parser in test_pdf_parsers:
+    timestamp = datetime.datetime.now()
     for pdf in dataset.pdfs:
         new_result = result()
         print(parser)
         results.append(parser.extract(pdf))
+    parser.metrics.speed = len(dataset.pdfs) / (datetime.datetime.now() - timestamp).total_seconds()
+    print("PDFs per second: ", parser.metrics.speed)
 
 #evaluate data
 def evaluate(extracted_data, pdf):
@@ -55,14 +63,10 @@ evaluations = []
 for extracted_data, pdf in zip(total_extracted_data, dataset.pdfs):
     evaluate(extracted_data, pdf)
 
-# notes for each
-for parser in pdf_parsers:
-    parser.metrics = metrics()
-
 pdf_plumber.metrics.speed = "very fast. TODO: number of pages per second"
 pdf_plumber.metrics.resource_efficiency = "very efficient: TODO CPU/GPU utilization metrics"
-pdf_plumber.metrics.cost = "Free"
 pdf_plumber.metrics.accuracy = "Accurate, from an eyeball test. TODO: use a comparison tool to compare with a ground truth"
+
 pdf_plumber.metrics.cost = "Free"
 PyPDF2.metrics.cost = "Free"
 PyPDF.metrics.cost = "Free"
@@ -73,6 +77,17 @@ camelot.metrics.cost = "Free"
 llama_parse.metrics.cost = "Paid"
 Amazon_Textract.metrics.cost = "Paid"
 Microsoft_Table_Transformer.metrics.cost = "Paid"
+
+pdf_plumber.metrics.speed
+PyPDF2.metrics.speed
+PyPDF.metrics.speed
+pdf_2_tables.metrics.speed
+tabula_py.metrics.speed
+PDFMiner.metrics.speed
+camelot.metrics.speed
+llama_parse.metrics.speed
+Amazon_Textract.metrics.speed
+Microsoft_Table_Transformer.metrics.speed
 
 # TODO print method creation
 # TODO use a python logger - one that can also save to a file
