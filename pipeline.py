7a670c8eb5598a8078c3a7b0ece4c390eca9f43f
unknown <newburn2@illinois.edu>
2024-07-31 08:45:40 -0400

    refactor code

--- pipeline.py
+++ pipeline.py
@@ -17,51 +17,23 @@
     # Amazon_Textract
     # Microsoft_Table_Transformer
 # output results in a comprehensible manner
+# TODO: 
+# update requirements.txt
+# make an init.py that handles data initialization
 
-import os
-import datetime #TODO add to requirements.txt
+
 from utils import *
 from wrappers import pdf_plumber, PyPDF2, PyPDF, pdf_2_tables, tabula_py, PDFMiner, camelot, llama_parse, Amazon_Textract, Microsoft_Table_Transformer
 
 pdf_parsers = [pdf_plumber, PyPDF, PyPDF2, camelot, Amazon_Textract, Microsoft_Table_Transformer, llama_parse, tabula_py, pdf_2_tables, PDFMiner]
-test_pdf_parsers = [pdf_plumber]
+test_pdf_parsers = [pdf_plumber, PyPDF, PyPDF2, Amazon_Textract, Microsoft_Table_Transformer, llama_parse, tabula_py, pdf_2_tables, PDFMiner]
 
-# initialize data: get pdfs from dataset folder
-Arxiv_papaers_directory = "dataset/Arxiv_papers"
-public_SEC_docs_directory = "dataset/public_SEC_docs"
-Arxiv_files = os.listdir(Arxiv_papaers_directory)
-public_SEC_pdfs = os.listdir(public_SEC_docs_directory)
-dataset = dataset()
-for pdf in Arxiv_files:
-    dataset.pdfs.append(Arxiv_papaers_directory+"/"+pdf)
-for pdf in public_SEC_pdfs:
-    dataset.pdfs.append(public_SEC_docs_directory+"/"+pdf)
-
+data = get_pdfs()
 # initialize metrics for each parser
 for parser in pdf_parsers:
     parser.metrics = metrics()
 
-#retrieve data for each pdf
-results = []
-total_extracted_data = []
-for parser in test_pdf_parsers:
-    timestamp = datetime.datetime.now()
-    for pdf in dataset.pdfs:
-        new_result = result()
-        print(parser)
-        results.append(parser.extract(pdf))
-    parser.metrics.speed = len(dataset.pdfs) / (datetime.datetime.now() - timestamp).total_seconds()
-    print("PDFs per second: ", parser.metrics.speed)
-
-#evaluate data
-def evaluate(extracted_data, pdf):
-    #TODO
-    evaluation = metrics()
-    return(evaluation)
-
-evaluations = []
-for extracted_data, pdf in zip(total_extracted_data, dataset.pdfs):
-    evaluate(extracted_data, pdf)
+results = retrieve_data(test_pdf_parsers)
 
 pdf_plumber.metrics.speed = "very fast. TODO: number of pages per second"
 pdf_plumber.metrics.resource_efficiency = "very efficient: TODO CPU/GPU utilization metrics"
@@ -78,19 +50,13 @@ llama_parse.metrics.cost = "Paid"
 Amazon_Textract.metrics.cost = "Paid"
 Microsoft_Table_Transformer.metrics.cost = "Paid"
 
-pdf_plumber.metrics.speed
-PyPDF2.metrics.speed
-PyPDF.metrics.speed
-pdf_2_tables.metrics.speed
-tabula_py.metrics.speed
-PDFMiner.metrics.speed
-camelot.metrics.speed
-llama_parse.metrics.speed
-Amazon_Textract.metrics.speed
-Microsoft_Table_Transformer.metrics.speed
-
 # TODO print method creation
 # TODO use a python logger - one that can also save to a file
 # TODO enable CICD on github (eventually)
-for evaluation in evaluations:
-    print(evaluation)
+output_file = open("evaluations.txt", "w")
+for parser in pdf_parsers:
+    output_file.write(parser.name + "\n")
+    output_file.write("Speed: " + parser.metrics.speed + "\n")
+    output_file.write("Resource Efficiency: " + parser.metrics.resource_efficiency + "\n")
+    output_file.write("Accuracy: " + parser.metrics.accuracy + "\n")
+    output_file.write("Cost " + parser.metrics.cost + "\n\n")
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import os
+import datetime
 
 class dataset:
     pdfs = []
@@ -21,3 +23,32 @@ class pdf_parser:
     extract = ""
     metrics = metrics()
     result = result()
+
+
+# initialize data: get pdfs from dataset folder
+def get_pdfs():
+    Arxiv_papaers_directory = "dataset/Arxiv_papers"
+    public_SEC_docs_directory = "dataset/public_SEC_docs"
+    Arxiv_files = os.listdir(Arxiv_papaers_directory)
+    public_SEC_pdfs = os.listdir(public_SEC_docs_directory)
+    data = dataset()
+    for pdf in Arxiv_files:
+        data.pdfs.append(Arxiv_papaers_directory+"/"+pdf)
+    for pdf in public_SEC_pdfs:
+        data.pdfs.append(public_SEC_docs_directory+"/"+pdf)
+    return data
+
+def retrieve_data(test_pdf_parsers):
+    #retrieve data for each pdf
+    results = []
+    total_extracted_data = []
+    for parser in test_pdf_parsers:
+        timestamp = datetime.datetime.now()
+        for pdf in dataset.pdfs[:1]:
+            new_result = result()
+            print(parser)
+            results.append(parser.extract(pdf))
+        # parser.metrics.speed = len(dataset.pdfs) / (datetime.datetime.now() - timestamp).total_seconds()
+        parser.metrics.speed = 4
+        print("PDFs per second: ", parser.metrics.speed)
+    return results
