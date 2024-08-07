668f2b684b40d8a459ad40f2d5f3cd3538648a19
unknown <newburn2@illinois.edu>
2024-08-07 08:46:00 -0400

    move output text to separate function

--- wrappers/pdf_plumber.py
+++ wrappers/pdf_plumber.py
@@ -4,13 +4,9 @@ name = "Pdf Plumber"
 
 def extract(pdf):
     plumber_pdf = pdfplumber.open(pdf)
-    output_dir = "output/"+pdf[:-4]
-    os.makedirs(output_dir, exist_ok=True)
-    output_file = open(output_dir+"/pdf_plumber.txt", "w", encoding='utf-8')
     text = ""
     for page in plumber_pdf.pages:
         page_text = page.extract_text()
-        output_file.write(page_text)
         text += page_text
         # extract only table data:
         # print(page.extract_tables(table_settings={}))
